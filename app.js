3e0b418d9b443fb4b2bb580447758c896397a5cb
Donizzy <techiedevjs@gmail.com>
2024-01-07 11:08:43 +0100

    closet shop

---
+++ app.js
@@ -0,0 +1,568 @@
+
+
+let categories = [
+    {
+        name: 'sports',
+        imageUrl: 'images/sports.png',
+        color: 'rgba(38, 80, 94, 0.40)'
+    },
+    {
+        name: 'casual',
+        imageUrl: 'images/casual.png',
+        color: 'rgba(169, 114, 31, 0.40)'
+    },
+    {
+        name: 'classic',
+        imageUrl: 'images/classic.png',
+        color: 'rgba(75, 53, 38, 0.40)'
+    },
+    {
+        name: 'k-pop',
+        imageUrl: 'images/k-pop.png',
+        color: 'rgba(71, 127, 151, 0.40)'
+    },
+    {
+        name: 'subculture',
+        imageUrl: 'images/subculture.png',
+        color: 'rgba(65, 29, 141, 0.40)'
+    },
+    {
+        name: 'glamour',
+        imageUrl: 'images/glamour.png',
+        color: 'rgba(193, 94, 100, 0.40)'
+    },
+    {
+        name: 'boho',
+        imageUrl: 'images/boho.png',
+        color: 'rgba(160, 138, 108, 0.40)'
+    },
+    {
+        name: 'vintage',
+        imageUrl: 'images/vintage.png',
+        color: 'rgba(160, 114, 46, 0.40)'
+    },
+    {
+        name: 'cosplay',
+        imageUrl: 'images/cosplay.png',
+        color: 'rgba(111, 82, 160, 0.40)'
+    },
+    {
+        name: 'sci-fi',
+        imageUrl: 'images/sci-fi.png',
+        color: 'rgba(160, 114, 46, 0.40)'
+    },
+    {
+        name: 'holiday',
+        imageUrl: 'images/holiday.png',
+        color: 'rgba(138, 21, 21, 0.40)'
+    },
+    {
+        name: 'medieval',
+        imageUrl: 'images/medieval.png',
+        color: 'rgba(41, 87, 58, 0.40)'
+    }
+]
+
+let clothes = [
+    {
+        id: 'top-1',
+        name: 'lucci',
+        type: 'printed t-shirt',
+        category: 'top',
+        price: 5,
+        imageUrl: 'images/top-1.png'
+    },
+    {
+        id: 'top-2',
+        name: 'gucci',
+        type: 'printed t-shirt',
+        category: 'top',
+        price: 10,
+        imageUrl: 'images/top-2.png'
+    },
+    {
+        id: 'top-3',
+        name: 'versace',
+        type: 'shirt and vest',
+        category: 'top',
+        price: 3,
+        imageUrl: 'images/top-3.png'
+    },
+    {
+        id: 'top-4',
+        name: 'calvin klein',
+        type: 'red jacket',
+        category: 'top',
+        price: 4,
+        imageUrl: 'images/top-4.png'
+    },
+    {
+        id: 'top-5',
+        name: 'kcl',
+        type: 'patterned shirt',
+        category: 'top',
+        price: 7,
+        imageUrl: 'images/top-5.png'
+    },
+    {
+        id: 'top-6',
+        name: 'amt',
+        type: 'winter jacket',
+        category: 'top',
+        price: 6,
+        imageUrl: 'images/top-6.png'
+    },
+    {
+        id: 'top-7',
+        name: 'tamx',
+        type: 'sweater',
+        category: 'top',
+        price: 3.5,
+        imageUrl: 'images/top-7.png'
+    },
+    {
+        id: 'top-8',
+        name: 'txmt',
+        type: 'beige jacket',
+        category: 'top',
+        price: 14,
+        imageUrl: 'images/top-8.png'
+    },
+    {
+        id: 'top-9',
+        name: 'amt',
+        type: 'classic shirt',
+        category: 'top',
+        price: 9,
+        imageUrl: 'images/top-9.png'
+    },
+    {
+        id: 'top-10',
+        name: 'amt',
+        type: 'printed t-shirt',
+        category: 'top',
+        price: 10,
+        imageUrl: 'images/top-10.png'
+    },
+    {
+        id: 'top-11',
+        name: 'amt',
+        type: 'brown jacket',
+        category: 'top',
+        price: 11,
+        imageUrl: 'images/top-11.png'
+    },
+    {
+        id: 'top-12',
+        name: 'amt',
+        type: 'graffiti jacket',
+        category: 'top',
+        price: 12,
+        imageUrl: 'images/top-12.png'
+    },
+    {
+        id: 'top-13',
+        name: 'amt',
+        type: 'printed t-shirt',
+        category: 'top',
+        price: 13,
+        imageUrl: 'images/top-13.png'
+    },
+    {
+        id: 'top-14',
+        name: 'amt',
+        type: 'winter jacket',
+        category: 'top',
+        price: 14,
+        imageUrl: 'images/top-14.png'
+    },
+    {
+        id: 'top-15',
+        name: 'amt',
+        type: 'printed t-shirt',
+        category: 'top',
+        price: 15,
+        imageUrl: 'images/top-15.png'
+    },
+    {
+        id: 'top-16',
+        name: 'amt',
+        type: 'printed t-shirt',
+        category: 'top',
+        price: 5,
+        imageUrl: 'images/top-1.png'
+    },
+    {
+        id: 'top-17',
+        name: 'amt',
+        type: 'printed t-shirt',
+        category: 'top',
+        price: 15,
+        imageUrl: 'images/top-5.png'
+    },
+    {
+        id: 'top-18',
+        name: 'amt',
+        type: 'printed t-shirt',
+        category: 'top',
+        price: 1.5,
+        imageUrl: 'images/top-7.png'
+    },
+    {
+        id: 'top-19',
+        name: 'amt',
+        type: 'printed t-shirt',
+        category: 'top',
+        price: 15,
+        imageUrl: 'images/top-10.png'
+    },
+    {
+        id: 'bottom-1',
+        name: 'amt',
+        type: 'pants',
+        category: 'bottom',
+        price: 15,
+        imageUrl: 'images/bottom-1.png'
+    },
+    {
+        id: 'bottom-2',
+        name: 'amt',
+        type: 'pants',
+        category: 'bottom',
+        price: 15,
+        imageUrl: 'images/bottom-2.png'
+    },
+    {
+        id: 'bottom-3',
+        name: 'amt',
+        type: 'pants',
+        category: 'bottom',
+        price: 15,
+        imageUrl: 'images/bottom-3.png'
+    },
+    {
+        id: 'bottom-4',
+        name: 'amt',
+        type: 'pants',
+        category: 'bottom',
+        price: 15,
+        imageUrl: 'images/bottom-4.png'
+    },
+    {
+        id: 'bottom-5',
+        name: 'amt',
+        type: 'pants',
+        category: 'bottom',
+        price: 15,
+        imageUrl: 'images/bottom-5.png'
+    },
+    {
+        id: 'bottom-6',
+        name: 'amt',
+        type: 'pants',
+        category: 'bottom',
+        price: 15,
+        imageUrl: 'images/bottom-6.png'
+    },
+    {
+        id: 'bottom-7',
+        name: 'amt',
+        type: 'pants',
+        category: 'bottom',
+        price: 15,
+        imageUrl: 'images/bottom-7.png'
+    },
+    {
+        id: 'bottom-8',
+        name: 'amt',
+        type: 'pants',
+        category: 'bottom',
+        price: 15,
+        imageUrl: 'images/bottom-8.png'
+    },
+    {
+        id: 'bottom-9',
+        name: 'amt',
+        type: 'pants',
+        category: 'bottom',
+        price: 15,
+        imageUrl: 'images/bottom-9.png'
+    },
+    {
+        id: 'bottom-10',
+        name: 'amt',
+        type: 'pants',
+        category: 'bottom',
+        price: 15,
+        imageUrl: 'images/bottom-10.png'
+    },
+]
+
+let outfits = [
+    {
+        id: 'outfit-1',
+        name: 'drip on drip',
+        imageUrl: 'images/outfit-1.png'
+    },
+    {
+        id: 'outfit-2',
+        name: 'corporate',
+        imageUrl: 'images/outfit-2.png'
+    },
+    {
+        id: '',
+        name: 'chillll',
+        imageUrl: 'images/outfit-3.png'
+    },
+    {
+        id: 'outfit-4',
+        name: 'cultural',
+        imageUrl: 'images/outfit-4.png'
+    },
+    {
+        id: 'outfit-5',
+        name: 'christmas cloth',
+        imageUrl: 'images/outfit-1.png'
+    },
+    {
+        id: 'outfit-6',
+        name: 'thief babe drips',
+        imageUrl: 'images/outfit-2.png'
+    },
+    {
+        id: 'outfit-7',
+        name: 'football club fit',
+        imageUrl: 'images/outfit-3.png'
+    },
+    {
+        id: 'outfit-8',
+        name: 'clubbing',
+        imageUrl: 'images/outfit-4.png'
+    },
+]
+
+let selectedType = 'top';
+document.querySelector('.category-title').innerHTML = selectedType;
+const pushCategories = (data) => {
+    document.querySelector('.clothing-categories').innerHTML = '';
+    data.map((category) => {
+        const  {name, imageUrl, color} = category;
+        document.querySelector('.clothing-categories').innerHTML += `
+        <div class="clothing-category" onclick="showMainShop()">
+            <img src=${imageUrl} alt="sports-category">
+            <div class="category-name" style="background-color:${color};">${name}</div>
+        </div>
+        `
+    })
+}
+const showMainShop = () => {
+    document.querySelector('.hero-container').classList.add('hide');
+    document.querySelector('.main-shop').classList.remove('hide');
+}
+const pushClothes = (data) => {
+    document.querySelector('.clothes').innerHTML = '';
+    data.map((cloth) => {
+        const {name, type, price, imageUrl, id, category} = cloth;
+        document.querySelector('.clothes').innerHTML += `
+        <div class="cloth ${category} ${category !== selectedType ? 'hide-item' : ''}" id=${id} onclick="viewCloth('${id}')">
+            <img src=${imageUrl} alt="cloth">
+            <div class="cloth-type">${type}</div>
+        </div>
+        `
+    })
+}
+const clearSelected = () => {
+    selectedClothId = ''
+    document.querySelector('.save-icon').classList.add('hide');
+    document.querySelectorAll('.cloth').forEach((elem) => elem.classList.remove('cloth-selected'));
+    document.querySelector('.cloth-details').classList.add('hide');
+    document.querySelector('.refresh').classList.add('hide');
+    document.querySelector('.choose-outfit-btn').style.opacity = 0.35
+    document.querySelector('.choose-outfit-btn').style.pointerEvents = 'none'
+}
+
+let selectedClothId = '';
+
+const viewCloth = (id) => {
+    let selected = document.getElementById(id);
+    if(selectedClothId !== id){
+        document.querySelector('.save-icon').classList.remove('hide')
+        selectedClothId = id
+        let cloth = clothes.filter((item) => item.id === id)[0];
+        document.querySelector('.cloth-details p').innerHTML = cloth.type;
+        document.querySelector('.cloth-details').classList.remove('hide');
+        selected.classList.add('cloth-selected');
+        let siblings = Array.from(selected.parentNode.children).filter((elem) => elem !== selected);
+        siblings.map((elem) => elem.classList.remove('cloth-selected'))
+        document.querySelector('.refresh').classList.remove('hide');
+        document.querySelector('.choose-outfit-btn').style.opacity = 1
+        document.querySelector('.choose-outfit-btn').style.pointerEvents = 'all'
+    } else {
+        clearSelected();
+    }
+}
+
+pushCategories(categories);
+pushClothes(clothes);
+// SAVED OUTFITS
+let start = 0;
+let end = 5;
+const pushOutfits = (data) => {
+    document.querySelector('.outfits').innerHTML = '';
+    data.slice(start, end).map((outfit) => {
+        const {imageUrl, name, id} = outfit;
+        document.querySelector('.outfits').innerHTML += `
+            <div class="outfit" id=${id}>
+                <img src=${imageUrl} alt="outfit">
+                <p>${name}</p>
+            </div>
+        `
+    })
+}
+pushOutfits(outfits)
+const Qcontrol = () => {
+    if(!document.querySelector('.saved-outfits-cont').classList.contains('hide')){
+        if(start === 0) {
+            start;
+            end;
+        } else {
+            start -= 1;
+            end -= 1;
+        }
+        pushOutfits(outfits);
+    }
+}
+
+document.addEventListener('keydown', evt => {
+    if (evt.key === 'q'){
+        Qcontrol();
+    }
+})
+
+const Econtrol = () => {
+    if(!document.querySelector('.saved-outfits-cont').classList.contains('hide')){
+        if(end === outfits.length) {
+            start;
+            end;
+        } else {
+            start += 1;
+            end += 1;
+        }
+        pushOutfits(outfits)
+    }
+}
+
+document.addEventListener('keydown', evt => {
+    if (evt.key === 'e'){
+        Econtrol()
+    }
+})
+const showSavedOutfits = () => {
+    document.querySelector('.saved-outfits-cont').classList.remove('hide');
+    document.querySelector('.rotate').classList.add('hide');
+    document.querySelector('.container').classList.add('hide')
+} 
+
+// SWITCH CLOTH CATEGORY/TYPE
+const switchTab = (tab) => {
+    document.querySelector('.cloth-details').classList.add('hide');
+    let selected = document.querySelector(`.${tab}`)
+    selectedType = tab;
+    document.querySelector('.category-title').innerHTML = selectedType;
+    selected.classList.add('selected-tab');
+    let siblings = Array.from(selected.parentNode.children).filter((elem) => elem !== selected);
+    siblings.map((elem) => elem.classList.remove('selected-tab'))
+    pushClothes(clothes)
+    document.getElementById(selectedClothId).classList.add('cloth-selected')
+}
+// CONFIRM PURCHASE POP UP AND CONFIRM FUNCTION
+const saveOutfitPopUp = () => {
+    document.querySelector('.save-outfit-cont').classList.remove('hide');
+    document.querySelector('.rotate').classList.add('hide');
+}
+
+// CLOSE POP-UPS FOR SAVE OUTFIT CONFIRMATIONS
+const cancelPopUp = () => {
+    document.querySelector('.save-outfit-cont').classList.add('hide');
+    document.querySelector('.rotate').classList.remove('hide');
+}
+let newOutfit = {
+    name : '',
+    id : selectedClothId,
+    imageUrl: 'images/outfit-1.png'
+}
+
+document.querySelector('.outfit-name').addEventListener('input', (e) => {
+    if(e.target.value){
+        newOutfit.name = e.target.value;
+        newOutfit.id = selectedClothId;
+        document.querySelector('.outfit-input').classList.add('active-input');
+        document.querySelector('.save-outfit-btn').style.opacity = 1;
+        document.querySelector('.save-outfit-btn').style.pointerEvents = 'all';
+    } else {
+        document.querySelector('.outfit-input').classList.remove('active-input');
+        document.querySelector('.save-outfit-btn').style.opacity = 0.35;
+        document.querySelector('.save-outfit-btn').style.pointerEvents = 'none';
+    }
+})
+
+const saveOutfit = () => {
+    outfits.push({...newOutfit})
+    console.log(newOutfit)
+    clearSelected();
+    back();
+}
+
+const chooseOutfit = () => {
+    console.log(selectedClothId);
+    clearSelected();
+}
+// FUNCTION BUTTONS - ROTATE AND BACK ESCAPE
+const rotateA = () => {
+    if(!document.querySelector('.rotate').classList.contains('hide')){
+        alert('rotate-A')
+    }
+}
+document.addEventListener('keydown', evt => {
+    if (evt.key === 'a'){
+        rotateA();
+    }
+})
+const rotateD = () => {
+    if(!document.querySelector('.rotate').classList.contains('hide')){
+        alert('rotate-D')
+    }
+}
+document.addEventListener('keydown', evt => {
+    if (evt.key === 'd'){
+        rotateD();
+    }
+})
+const back = () => {
+    if (!document.querySelector('.saved-outfits-cont').classList.contains('hide')){
+        document.querySelector('.saved-outfits-cont').classList.add('hide');
+        document.querySelector('.rotate').classList.remove('hide');
+        document.querySelector('.container').classList.remove('hide')    
+    } else if(!document.querySelector('.save-outfit-cont').classList.contains('hide')){
+        cancelPopUp()
+    } else  if (document.querySelector('.main-shop').classList.contains('hide')) {
+        document.querySelector('.display').classList.add('hide')
+    } else {
+        document.querySelector('.main-shop').classList.add('hide');
+        document.querySelector('.hero-container').classList.remove('hide');
+    }
+}
+document.addEventListener('keydown', evt => {
+    if(evt.key === 'Escape'){
+        back()
+    }
+})
+// TOGGLE DISPLAY WITH HOME KEY
+const toggleDisplay = () => {
+    document.querySelector('.display').classList.toggle('hide')
+}
+document.addEventListener('keydown', evt => {
+    if (evt.key === 'Home') {
+        toggleDisplay();
+    }
+});
